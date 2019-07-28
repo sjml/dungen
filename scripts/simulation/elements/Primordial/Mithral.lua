b3040a985cd0d075acc8dde37095223015c0b986
Shane Liesegang <shane@techie.net>
2019-07-28 16:37:20 +0200

    simple is better than complex; flat is better than nested

--- scripts/simulation/elements/Primordial/Mithral.lua
+++ scripts/simulation/elements/Primordial/Mithral.lua
@@ -12,19 +12,14 @@ local mithralSolver = ConstraintSolver(mithralConstraints)
 mithralSolver:Solve()
 
 local base = mithralSolver.pickedTile
-local mithralPit = {}
-table.insert(mithralPit, base)
+base:AddTag("mithral")
 
 if (base.hexPos.y > (gir("GroundHeight") / 2)) then
   -- bottom part of the field; face down
-  table.insert(mithralPit, GetTileAtIndex(base.neighborNE))
-  table.insert(mithralPit, GetTileAtIndex(base.neighborNW))
+  GetTileAtIndex(base.neighborNE):AddTag("mithral")
+  GetTileAtIndex(base.neighborNW):AddTag("mithral")
 else
   -- top part of the field; face up
-  table.insert(mithralPit, GetTileAtIndex(base.neighborSE))
-  table.insert(mithralPit, GetTileAtIndex(base.neighborSW))
-end
-
-for _, m in pairs(mithralPit) do
-  m:AddTag("mithral")
+  GetTileAtIndex(base.neighborSE):AddTag("mithral")
+  GetTileAtIndex(base.neighborSW):AddTag("mithral")
 end
