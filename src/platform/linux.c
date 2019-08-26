0deead0689402ee9546bd438df2459e3c8f970e4
Shane Liesegang <shane@techie.net>
2019-08-26 16:47:40 -0700

    let's see if this works

--- src/platform/linux.c
+++ src/platform/linux.c
@@ -1,6 +1,12 @@
 #include "../stdafx.h"
 
+#include <unistd.h>
+
 void InitializePlatform() {
+    if (chdir("./Resources") != 0) {
+		fprintf(stderr, "ERROR: Could not find Resources directory. Exiting.\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 void FinalizePlatform() {
