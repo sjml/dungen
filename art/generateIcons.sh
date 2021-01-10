8af4b8356cd153f3b170c99c79e941b3139ed2db
Shane Liesegang <shane@techie.net>
2021-01-10 14:37:26 +0200

    keep pixel look on scaled icons

--- art/generateIcons.sh
+++ art/generateIcons.sh
@@ -9,7 +9,7 @@ MAC_OUTPUT_DIR="mac_appiconset"
 rm -rf $MAC_OUTPUT_DIR
 mkdir -p $MAC_OUTPUT_DIR
 for s in ${MAC_SIZES[@]}; do
-  convert $INPUT -resize $sx$s $MAC_OUTPUT_DIR/$s.png
+  convert $INPUT -filter point -resize $sx$s $MAC_OUTPUT_DIR/$s.png
 done
 
 #iOS
@@ -18,11 +18,11 @@ IOS_OUTPUT_DIR="ios_appiconset"
 rm -rf $IOS_OUTPUT_DIR
 mkdir -p $IOS_OUTPUT_DIR
 for s in ${IOS_SIZES[@]}; do
-  convert $INPUT -resize $sx$s $IOS_OUTPUT_DIR/$s.png
+  convert $INPUT -filter point -resize $sx$s $IOS_OUTPUT_DIR/$s.png
 done
 
 # Windows
 WIN_SIZES=(256 128 96 64 48 32 16)
 sizeList=$(printf ",%s" "${WIN_SIZES[@]}")
 sizeList=${sizeList:1}
-convert $INPUT -resize 256 -define icon:auto-resize="$sizeList" $BASE.ico
+convert $INPUT -filter point -resize 256 -define icon:auto-resize="$sizeList" $BASE.ico
