98d837f8c3d6bfb308ffb0266bb6c5b1af7adebf
Shane Liesegang <shane@techie.net>
2021-01-11 17:35:28 +0200

    web build + deploy

--- buildscripts/buildWebVersion.sh
+++ buildscripts/buildWebVersion.sh
@@ -9,7 +9,7 @@ source ~/emsdk/emsdk_env.sh
 pushd ../build
 
 emcmake cmake -DCMAKE_BUILD_TYPE=MinSizeRel ..
-cmake --build .
+cmake --build . --parallel $(nproc)
 mkdir -p site
 mv DunGen.{html,js,wasm,data} ./site
 mv ./site/DunGen.html ./site/index.html
