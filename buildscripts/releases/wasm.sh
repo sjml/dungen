69fe32922e554cabf767da4cdb624efe3de7a51b
Shane Liesegang <shane@techie.net>
2021-10-10 17:22:44 +0300

    extracting specific paths

--- buildscripts/releases/wasm.sh
+++ buildscripts/releases/wasm.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 cd ../..
 
 meson \
-  --cross-file platform/WebAssembly/wasm-cross-constants.txt \
+  --cross-file local/wasm-cross-constants.txt \
   --cross-file platform/WebAssembly/wasm-cross.txt \
   --buildtype=release \
   --prefix=$(pwd)/build/DunGen.dist \
@@ -18,7 +18,7 @@ meson compile -C build
 # reconfigure step is silly, but Meson is a bit over-opinionated and it's the
 #   only way to get it to see the extra files emitted by Emscripten (the html and data)
 meson \
-  --cross-file platform/WebAssembly/wasm-cross-constants.txt \
+  --cross-file local/wasm-cross-constants.txt \
   --cross-file platform/WebAssembly/wasm-cross.txt \
   --buildtype=release \
   --prefix=$(pwd)/build/DunGen.dist \
