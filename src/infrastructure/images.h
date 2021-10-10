5f67f2d050ab85754c47b743446bdd1f90c616c3
Shane Liesegang <shane@techie.net>
2021-10-10 22:25:56 +0300

    windows taskbar icon

--- src/infrastructure/images.h
+++ src/infrastructure/images.h
@@ -1,6 +1,7 @@
 #pragma once
 
 
+unsigned char* GetRawImageData(const char* filename, unsigned int *dataLength, int *texWidth, int *texHeight);
 void LoadColorTable(const char* tablename, const char* filename);
 gbVec3 GetColorByIndex(const char* tablename, int i);
 gbVec3 GetColorByPercent(const char* tablename, float p);
--- src/platform/windows.c
+++ src/platform/windows.c
@@ -3,16 +3,34 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#include "../infrastructure/images.h"
+
 void InitializePlatform() {
 
-#ifdef _DEBUG
-    SetCurrentDirectoryA(".\\Resources");
-#else
-    if (SetCurrentDirectoryA(".\\Resources") == 0) {
-        fprintf(stderr, "ERROR: Could not find Resources directory. Exiting.\n");
-        exit(EXIT_FAILURE);
-    }
-#endif
+    #ifdef _DEBUG
+        SetCurrentDirectoryA(".\\Resources");
+    #else
+        if (SetCurrentDirectoryA(".\\Resources") == 0) {
+            fprintf(stderr, "ERROR: Could not find Resources directory. Exiting.\n");
+            exit(EXIT_FAILURE);
+        }
+    #endif
+
+    unsigned int smallDataLength, smallImageWidth, smallImageHeight;
+    const unsigned char* small = GetRawImageData("images/appicons/128.png", &smallDataLength, &smallImageWidth, &smallImageHeight);
+    unsigned int mediumDataLength, mediumImageWidth, mediumImageHeight;
+    const unsigned char* medium = GetRawImageData("images/appicons/256.png", &mediumDataLength, &mediumImageWidth, &mediumImageHeight);
+    unsigned int largeDataLength, largeImageWidth, largeImageHeight;
+    const unsigned char* large = GetRawImageData("images/appicons/512.png", &largeDataLength, &largeImageWidth, &largeImageHeight);
+
+    const sapp_icon_desc icon_desc = {
+        .images = {
+            { .width = 128, .height = 128, .pixels = { .ptr = small,  .size = smallDataLength  * sizeof(unsigned char)} },
+            { .width = 256, .height = 256, .pixels = { .ptr = medium, .size = mediumDataLength * sizeof(unsigned char)} },
+            { .width = 512, .height = 512, .pixels = { .ptr = large,  .size = largeDataLength  * sizeof(unsigned char)} },
+        }
+    };
+    sapp_set_icon(&icon_desc);
 }
 
 void FinalizePlatform() {
