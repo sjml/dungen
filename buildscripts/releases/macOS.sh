35a599a2d60a4e44230e9765f6d80758ad99a439
Shane Liesegang <shane@techie.net>
2021-10-11 15:06:46 +0300

    fixing mac release script

--- buildscripts/releases/macOS.sh
+++ buildscripts/releases/macOS.sh
@@ -5,13 +5,14 @@ set -e
 cd "$(dirname "$0")"
 cd ../..
 
-meson --buildtype=release --prefix=$(pwd)/build/Dungen.dist/DunGen.app --bindir=Contents/MacOS build
+meson --buildtype=release --prefix=$(pwd)/build/DunGen.dist/DunGen.app --bindir=Contents/MacOS build
 meson compile -C build
 cd build
 meson install
+cd DunGen.dist
 codesign \
   --sign CFJYV723M9 \
-  --entitlements ../platform/macOS/entitlements.plist \
+  --entitlements ../../platform/macOS/entitlements.plist \
   --options runtime \
   --timestamp \
   DunGen.app
