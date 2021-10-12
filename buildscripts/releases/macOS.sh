05a75c04346271fc3a5a568c0cfe810e99f689cc
Shane Liesegang <shane@techie.net>
2021-10-12 12:55:05 +0300

    don't codesign for now until CI is set

--- buildscripts/releases/macOS.sh
+++ buildscripts/releases/macOS.sh
@@ -12,12 +12,12 @@ meson compile -C build
 cd build
 meson install
 cd DunGen_dist
-codesign \
-  --sign CFJYV723M9 \
-  --entitlements ../../platform/macOS/entitlements.plist \
-  --options runtime \
-  --timestamp \
-  DunGen.app
+# codesign \
+#   --sign CFJYV723M9 \
+#   --entitlements ../../platform/macOS/entitlements.plist \
+#   --options runtime \
+#   --timestamp \
+#   DunGen.app
 # TODO: notarize
 cd ../..
 
